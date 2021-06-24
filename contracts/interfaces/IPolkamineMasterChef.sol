44840f7a2663dc36af1849e37d057d5e3c20ee18
buckdy <buck3dy@gmail.com>
2021-06-24 11:03:15 +1000

    add PolkaminePoolManager

--- contracts/interfaces/IPolkamineMasterChef.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-abstract contract IPolkamineMasterChef {
-  function addPool(address _pAddress) external virtual returns (uint256);
-
-  function removePool(uint256 _pid) external virtual;
-}
---
+++ contracts/interfaces/IPolkaminePoolManager.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+abstract contract IPolkaminePoolManager {
+  function addPool(address _pool) external virtual returns (uint256);
+
+  function removePool(uint256 _pid) external virtual returns (address);
+
+  function allPools() external virtual returns (address[] memory);
+}
